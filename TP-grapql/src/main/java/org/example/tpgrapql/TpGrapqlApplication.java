cb437dd3e2feb544630036e642c18246f19d4b74
Tacinaa <tasnimeyahiaoui59@gmail.com>
2025-11-27 12:52:30 +0100

    Convertir TP-grapql de sous-module en dossier normal

---
+++ TP-grapql/src/main/java/org/example/tpgrapql/TpGrapqlApplication.java
@@ -0,0 +1,13 @@
+package org.example.tpgrapql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpGrapqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpGrapqlApplication.class, args);
+    }
+
+}
---
+++ TP-grapql/src/test/java/org/example/tpgrapql/TpGrapqlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tpgrapql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpGrapqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
